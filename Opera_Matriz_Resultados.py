094f8e9651ef7a23887170802f3e33c1784b6b1b
ivanzrp <ivan.zimmerp@gmail.com>
2024-12-25 23:45:32 -0300

    Corrige algunos errores de simulaciones a diputados, se crea primera simulacion de senadores modificando el codigo para ello y se reordena carpeta para incorporar resultados

--- Opera_Matriz_Resultados.py
+++ Opera_Matriz_Resultados.py
@@ -49,27 +49,34 @@ def leer_csv_desde_drive(url):
 
     return df
 
-url_comunas_distro=r"https://drive.google.com/uc?id=1SGJXB8iu7384-3a94mV2QFjTMfjbeVpL"
-comunas_distrito=leer_excel_desde_drive(url_comunas_distro)
+#url para eleccion de diputados
+#url_comunas=r"https://drive.google.com/uc?id=1SGJXB8iu7384-3a94mV2QFjTMfjbeVpL"
+#url para eleccion de senadores
+url_comunas=r"https://drive.google.com/uc?id=1lhFt_SBBUDEPn-eJ84xkaLGBxAl7P8J3"
+territorios=leer_excel_desde_drive(url_comunas)
 resultados_proyectados=pd.read_csv("resultados_proyectados.csv", encoding= 'utf-8',sep=';')
 # Eliminar todas las columnas cuyo nombre contenga "Unnamed"
 resultados_proyectados = resultados_proyectados.loc[:, ~resultados_proyectados.columns.str.contains('^Unnamed')]
-comunas_distrito = comunas_distrito.set_index('comuna')
+territorios = territorios.set_index('comuna')
 resultados_proyectados = resultados_proyectados.set_index('Comuna')
 
-# Paso 2: Ahora unimos 'resultados_proyectados_transpuesto' con la columna 'Distrito' de 'comunas_distrito'
-resultados_proyectados['Distrito'] = resultados_proyectados.index.map(comunas_distrito['Distrito'])
+# Paso 2: Ahora unimos 'resultados_proyectados_transpuesto' con la columna 'Distrito' o 'Circunscripcion' de 'territorios' según la eleccion
+#resultados_proyectados['Distrito'] = resultados_proyectados.index.map(territorios['Distrito'])
+resultados_proyectados['Circunscripcion'] = resultados_proyectados.index.map(territorios['Circunscripcion'])
 
-# Paso 3: Agrupar por 'Distrito' y sumar los votos de cada partido
-resultados_proyectados_distrito = resultados_proyectados.groupby('Distrito').sum()
-resultados_proyectados_distrito.index = resultados_proyectados_distrito.index.astype(int)
+
+# Paso 3: Agrupar por 'Distrito' o 'Circunscripcion' y sumar los votos de cada partido
+#resultados_proyectados_territorio = resultados_proyectados.groupby('Distrito').sum()
+resultados_proyectados_territorio = resultados_proyectados.groupby('Circunscripcion').sum()
+
+resultados_proyectados_territorio.index = resultados_proyectados_territorio.index.astype(int)
 # Paso 1: Calcular la suma de votos por fila y almacenarlo en una nueva columna
-resultados_proyectados_distrito['total_votos'] = resultados_proyectados_distrito.sum(axis=1)
+resultados_proyectados_territorio['total_votos'] = resultados_proyectados_territorio.sum(axis=1)
 
 # Paso 2: Dividir cada valor de la fila por el total de votos de la misma fila
-resultados_proyectados_distrito_normalizado = resultados_proyectados_distrito.div(resultados_proyectados_distrito['total_votos'], axis=0)
+resultados_proyectados_territorio_normalizado = resultados_proyectados_territorio.div(resultados_proyectados_territorio['total_votos'], axis=0)
 
 # Paso 3: Eliminar la columna 'total_votos' del DataFrame normalizado (si no la necesitas)
-resultados_proyectados_distrito_normalizado = resultados_proyectados_distrito_normalizado.drop(columns=['total_votos'])
+resultados_proyectados_territorio_normalizado = resultados_proyectados_territorio_normalizado.drop(columns=['total_votos'])
 
-resultados_proyectados_distrito_normalizado.to_csv("resultados_proyectados_proporciones.csv", encoding= 'utf-8',sep=';')
+resultados_proyectados_territorio_normalizado.to_csv("resultados_proyectados_proporciones.csv", encoding= 'utf-8',sep=';')
